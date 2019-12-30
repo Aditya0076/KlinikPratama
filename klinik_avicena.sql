060b99c95530a9cf94e04d27b7a3723a95432498
Aditya0076 <aditya07bayu@gmail.com>
2019-12-30 15:03:12 +0700

    Add SQL

---
+++ klinik_avicena.sql
@@ -0,0 +1,251 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 30 Des 2019 pada 08.33
+-- Versi server: 10.3.16-MariaDB
+-- Versi PHP: 7.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `klinik_avicena`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `nama_admin` varchar(50) NOT NULL,
+  `umur` int(11) NOT NULL,
+  `jabatan` varchar(32) NOT NULL,
+  `alamat` text NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data_pasien`
+--
+
+CREATE TABLE `data_pasien` (
+  `kode_pasien` int(11) NOT NULL,
+  `kode_keluarga` int(11) NOT NULL,
+  `nama_pasien` varchar(50) NOT NULL,
+  `umur` varchar(2) NOT NULL,
+  `jenis_kelamin` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `dusun`
+--
+
+CREATE TABLE `dusun` (
+  `kode_dusun` int(3) NOT NULL,
+  `nama_dusun` varchar(30) NOT NULL,
+  `nama_desa` varchar(64) NOT NULL,
+  `jabatan` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kepala_keluarga`
+--
+
+CREATE TABLE `kepala_keluarga` (
+  `kode_keluarga` int(11) NOT NULL,
+  `kode_dusun` int(11) NOT NULL,
+  `nama_kepala` varchar(50) NOT NULL,
+  `rt` varchar(3) NOT NULL,
+  `alamat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `obat`
+--
+
+CREATE TABLE `obat` (
+  `id_obat` int(11) NOT NULL,
+  `nama_obat` varchar(64) NOT NULL,
+  `jenis_obat` varchar(64) NOT NULL,
+  `harga_obat` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `rekam_medis`
+--
+
+CREATE TABLE `rekam_medis` (
+  `kode_rekam` int(11) NOT NULL,
+  `kode_pasien` int(11) NOT NULL,
+  `id_obat` int(11) NOT NULL,
+  `waktu` varchar(50) NOT NULL,
+  `ciri` text NOT NULL,
+  `diagnosa` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `harga_transaksi` int(11) NOT NULL,
+  `id_obat` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`jabatan`);
+
+--
+-- Indeks untuk tabel `data_pasien`
+--
+ALTER TABLE `data_pasien`
+  ADD PRIMARY KEY (`kode_pasien`),
+  ADD KEY `fk_kode_keluarga` (`kode_keluarga`);
+
+--
+-- Indeks untuk tabel `dusun`
+--
+ALTER TABLE `dusun`
+  ADD PRIMARY KEY (`kode_dusun`),
+  ADD KEY `fk_kode_jabatan` (`jabatan`);
+
+--
+-- Indeks untuk tabel `kepala_keluarga`
+--
+ALTER TABLE `kepala_keluarga`
+  ADD PRIMARY KEY (`kode_keluarga`),
+  ADD KEY `fk_kode_dusun` (`kode_dusun`);
+
+--
+-- Indeks untuk tabel `obat`
+--
+ALTER TABLE `obat`
+  ADD PRIMARY KEY (`id_obat`);
+
+--
+-- Indeks untuk tabel `rekam_medis`
+--
+ALTER TABLE `rekam_medis`
+  ADD PRIMARY KEY (`kode_rekam`),
+  ADD KEY `fk_kode_pasien` (`kode_pasien`),
+  ADD KEY `rekam_medis_ibfk_1` (`id_obat`);
+
+--
+-- Indeks untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `fk_id_obat` (`id_obat`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `data_pasien`
+--
+ALTER TABLE `data_pasien`
+  MODIFY `kode_pasien` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `dusun`
+--
+ALTER TABLE `dusun`
+  MODIFY `kode_dusun` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `kepala_keluarga`
+--
+ALTER TABLE `kepala_keluarga`
+  MODIFY `kode_keluarga` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `obat`
+--
+ALTER TABLE `obat`
+  MODIFY `id_obat` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `rekam_medis`
+--
+ALTER TABLE `rekam_medis`
+  MODIFY `kode_rekam` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `data_pasien`
+--
+ALTER TABLE `data_pasien`
+  ADD CONSTRAINT `fk_kode_keluarga` FOREIGN KEY (`kode_keluarga`) REFERENCES `kepala_keluarga` (`kode_keluarga`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `dusun`
+--
+ALTER TABLE `dusun`
+  ADD CONSTRAINT `fk_kode_jabatan` FOREIGN KEY (`jabatan`) REFERENCES `admin` (`jabatan`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `kepala_keluarga`
+--
+ALTER TABLE `kepala_keluarga`
+  ADD CONSTRAINT `fk_kode_dusun` FOREIGN KEY (`kode_dusun`) REFERENCES `dusun` (`kode_dusun`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `rekam_medis`
+--
+ALTER TABLE `rekam_medis`
+  ADD CONSTRAINT `fk_kode_pasien` FOREIGN KEY (`kode_pasien`) REFERENCES `data_pasien` (`kode_pasien`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `rekam_medis_ibfk_1` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id_obat`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `fk_id_obat` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id_obat`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
