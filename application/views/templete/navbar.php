9fc5ef68a83dc3df1dc43af6435571dabf41d15c
Aditya0076 <aditya07bayu@gmail.com>
2020-01-04 00:55:31 +0700

    penambahan link navbar

--- application/views/templete/navbar.php
+++ application/views/templete/navbar.php
@@ -4,37 +4,37 @@
 <div class="navbar navbar-expand-sm navbar-dark" style="width: 1266px;height: 82px;left: 46px;top: 45px; background: #1C3387;border-radius: 20px;">
 	<ul class="navbar-nav m-auto">
 		<li class="nav-item " style="color: ghostwhite;">
-			<a class="nav-link" href="#">Beranda</a>
+			<a class="nav-link" href="<?= base_url('');?>petugas/read">Beranda</a>
 		</li>
 		<li class="nav-item dropdown" style="color: ghostwhite;">
 			<a class="nav-link dropdown-toggle " href="#" id="navbardrop" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 				Data Pasien
 			</a>
 			<div class="dropdown-menu"aria-labelledby="navbarDropdown">
-				<a class="dropdown-item" href="#">Tambah Dusun</a>
-				<a class="dropdown-item" href="#">Tambah Kepala Keluarga</a>
-				<a class="dropdown-item" href="#">Tambah Pasien</a>
-				<a class="dropdown-item" href="#">Tampil Data Pasien</a>
+				<a class="dropdown-item" href="<?= base_url('');?>dusun/create">Tambah Dusun</a>
+				<a class="dropdown-item" href="<?= base_url('');?>kepala_keluarga/create">Tambah Kepala Keluarga</a>
+				<a class="dropdown-item" href="<?= base_url('');?>pasien/create">Tambah Pasien</a>
+				<a class="dropdown-item" href="<?= base_url('');?>pasien">Tampil Data Pasien</a>
 			</div>
 		</li>
 		<li class="nav-item dropdown"style="color: ghostwhite;">
-			<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+			<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 				Data Medis
 			</a>
-			<div class="dropdown-menu">
-				<a class="dropdown-item" href="#">Tambah Data Obat</a>
-				<a class="dropdown-item" href="#">Tampil Data Obat</a>
-				<a class="dropdown-item" href="#">Tambah Data Rekam</a>
-				<a class="dropdown-item" href="#">Tampil Data Rekam</a>
+			<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+				<a class="dropdown-item" href="<?= base_url('');?>obat/create">Tambah Data Obat</a>
+				<a class="dropdown-item" href="<?= base_url('');?>obat">Tampil Data Obat</a>
+				<a class="dropdown-item" href="<?= base_url('');?>rekam_medis/create">Tambah Data Rekam</a>
+				<a class="dropdown-item" href="<?= base_url('');?>rekam_medis">Tampil Data Rekam</a>
 			</div>
 		</li>
 		<li class="nav-item dropdown" style="color: ghostwhite;">
-			<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+			<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 				Laporan
 			</a>
-			<div class="dropdown-menu">
-				<a class="dropdown-item" href="#">Tambah Data Transaksi</a>
-				<a class="dropdown-item" href="#">Tampil Data Transaksi</a>
+			<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+				<a class="dropdown-item" href="<?= base_url('');?>transaksi/create">Tambah Data Transaksi</a>
+				<a class="dropdown-item" href="<?= base_url('');?>transaksi">Tampil Data Transaksi</a>
 			</div>
 		</li>
 		<li class="nav-item" style="color: ghostwhite;">
